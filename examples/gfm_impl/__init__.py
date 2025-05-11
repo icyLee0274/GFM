33545111e3c2561cddcc58b5037a25f7f9d8dd2e
icylee <icylee074@gmail.com>
2025-05-11 23:06:05 +0800

    Star convex example

--- examples/gfm_impl/__init__.py
+++ examples/gfm_impl/__init__.py
@@ -1,3 +1,4 @@
 from .polytope import Polytope
 from .qc import Quadratic
 from .compound_2d import Compound2D
+from .star_convex import Star, StarDomain
---
+++ examples/gfm_impl/star_convex.py
@@ -0,0 +1,62 @@
+from omegaconf import DictConfig
+import torch, numpy as np
+from torch import Tensor
+
+import gfm
+import examples
+from gfm import ConstrainedSet
+
+
+class StarDomain(gfm.ConstrainedSet):
+
+    def __init__(self, alpha: float, n_tips: int):
+        super().__init__()
+        self.alpha = alpha
+        self.n_tips = n_tips
+
+    def gamma(self, xs: Tensor) -> Tensor:
+        return 1 + self.alpha * torch.sin(self.n_tips * torch.atan2(xs[:, 1], xs[:, 0]))
+
+    def check_feasibility_v(self, points: Tensor, device=torch.get_default_device()) -> Tensor:
+        lhs = torch.linalg.vector_norm(points, dim=-1)
+        rhs = self.gamma(points)
+        return lhs <= rhs
+
+    def eval_intersection_v(
+            self,
+            os: Tensor, vs: Tensor,
+            tol: float = 1e-6, thresh: float = 1e8,
+            device=torch.get_default_device()) -> Tensor:
+        return 1 / self.gamma(vs)
+
+
+class Star(examples.GfmExampleBase):
+
+    def __init__(self, cfg: DictConfig):
+        super().__init__(cfg)
+
+    def _init_domain(self) -> tuple[ConstrainedSet, Tensor]:
+        return StarDomain(self.cfg.example.alpha, self.cfg.example.n_tips), \
+            torch.zeros(2, device=self.cfg.accelerator, dtype=torch.float32)
+
+    @torch.no_grad()
+    def _init_data(self, n: int) -> Tensor:
+        dim = 2
+        device = self.cfg.accelerator
+
+        thetas = torch.linspace(0, 2 * torch.pi, self.cfg.example.n_tips + 1, device=device)[:-1]
+        avgs = torch.stack([
+            torch.cos(thetas),
+            torch.sin(thetas),
+        ], dim=1)
+
+        cov = (self.cfg.example.scale * torch.eye(dim, device=device))
+
+        dists = [torch.distributions.MultivariateNormal(torch.zeros(dim, device=device), cov)]
+        for avg in avgs:
+            dists.append(torch.distributions.MultivariateNormal(avg, cov))
+
+        data_dist = gfm.TruncatedDistribution(gfm.SumDistribution(*dists), self.get_domain())
+        data = data_dist.sample(torch.Size([n]))
+
+        return data
