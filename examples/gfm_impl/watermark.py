4b23d7adada156264600f0349e63418c14f4402c
icylee <icylee074@gmail.com>
2025-07-31 17:45:59 +0800

    Bug fix: watermark mat dim

--- examples/gfm_impl/watermark.py
+++ examples/gfm_impl/watermark.py
@@ -84,7 +84,7 @@ class Watermark(examples.GfmExampleBase):
         At = torch.cat([basis, -basis], dim=1)
         b = torch.cat([torch.full([cons], upper, device=self.device),
                        torch.full([cons], -lower, device=self.device)], dim=0)
-        self.register_buffer('basis', torch.cat([basis[:n_pixels, :], -basis[:n_pixels, :]], dim=0))
+        self.register_buffer('basis', torch.cat([basis[:n_pixels, :], -basis[:n_pixels, :]], dim=1))
         self.register_buffer('bounds', b)
         domain = gfm.LinearConstraint(b=b, At=At, box_lower=-1.0, box_upper=1.0)
 
